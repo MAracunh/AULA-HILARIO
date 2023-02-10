d02985e3b260d47cea491cc31f06ae5143a1e2cf
Matheus Cunha <117128503+MAracunh@users.noreply.github.com>
2023-02-10 00:59:17 +0000

    minha aula hilario

---
+++ Aula02.java
@@ -0,0 +1,9 @@
+public class Aula02
+{
+    public static void main(String[] args)
+    {
+        String nome;
+        nome = "Teste";
+        System.out.println(nome);
+    }
+}
---
+++ Aula02_01.java
@@ -0,0 +1,13 @@
+public class Aula02_01
+{
+    public static void main(String[] args)
+    {
+        int contador = 0;
+
+        while(contador < 100)
+        {
+            System.out.println("Boa noite");
+            contador++;
+        }
+    }
+}
